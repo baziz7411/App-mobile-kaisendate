c1e5868f051184ad2321e29484cc5d999bf4204a
gabylim <bennacer.nabila96@gmail.com>
2022-06-13 21:07:34 +0200

    suite des interfaces

---
+++ app/src/main/java/com/example/testkotlin/MainActivity1.kt
@@ -0,0 +1,22 @@
+package com.example.testkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+        val  text: Button =findViewById(R.id.start)
+
+
+
+
+
+
+
+
+    }
+}
